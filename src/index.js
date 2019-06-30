4f503da84f7047684f06ba9066ff1e26da142d7b
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2019-07-01 00:03:01 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import h from 'hyperscript';
+import hh from 'hyperscript-helpers';
+
+const { div, button } = hh(h);
+
+const initModel = 0;
+
+function view(model) {
+  return div([
+    div({ className: 'mv2' }, `Count: ${model}`),
+    button({ className: 'pv1 ph2 mr2', onclick: () => console.log('+ clicked')},'+'),
+    button({className: 'pv1 ph2', onclick: () => console.log('- clicked')},'-')
+  ]);
+}
+
+function update(msg, model) {
+  switch (msg) {
+    case 'plus':
+      return model + 1;
+    case 'minus':
+      return model - 1;
+    default:
+      return model;
+  }
+}
+
+const rootNode = document.getElementById('app');
+rootNode.appendChild(view(update('minus', initModel)));
